46ec6dbd29aab898c762c819a1d2f03b779685b8
Johannes Holland <joh.ho@gmx.de>
2020-11-07 20:52:07 +0100

    linters: silent on success

--- linters.sh
+++ linters.sh
@@ -4,20 +4,32 @@ module="tpm2_gui"
 files="$(find ${module} -name '*.py' | xargs)"
 license_tmplate="setup.py"
 
+exitcode=0
+
 # Check that licenses are consistent
+echo "################################ LICENSE ################################"
 license=$(sed -n '/#/,/^$/p' ${license_tmplate})
 license_lines=$(echo "${license}" | wc -l)
 for f in ${files}; do
     f_license="$(head -n${license_lines} ${f})"
     if [ "${f_license}" != "${license}" ]; then
         echo "ERROR: License does not match ${license_tmplate}: ${f}" >&2
-        exit 1
+        exitcode=1
     fi
 done
 
-isort ${files}
-black ${module}
-pylint ${module}
-mypy ${module}  #TODO
-flake8 ${module}
-bandit -r ${module}
+echo "################################# ISORT #################################"
+isort ${files} || exitcode=1
+echo "################################# BLACK #################################"
+black --quiet --check --diff ${module} || exitcode=1
+# black ${module} 2>&1  | grep -P '^\d+ files left unchanged.' || echo error
+echo "################################# PYLINT ################################"
+pylint --score=n ${module} || exitcode=1
+# echo "################################## MYPY #################################"
+#mypy ${module}  || exitcode=1 #TODO
+echo "################################# FLAKE8 ################################"
+flake8 ${module} || exitcode=1
+echo "################################# BANDIT ################################"
+bandit --quiet --recursive ${module} || exitcode=1
+
+test "${exitcode}" = 0
